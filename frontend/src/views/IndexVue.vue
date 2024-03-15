3c69f398595fbb14bd49da374599ffe6fa61807a
zy <1824762582@qq.com>
2024-03-15 18:23:20 +0800

    bootstrap的导入

--- frontend/src/views/IndexVue.vue
+++ frontend/src/views/IndexVue.vue
@@ -4,10 +4,30 @@
 
 <template>
   <div style="position: fixed; top: 0; left: 0; width: 100%;">
-    7777
-    7777
-
-    4它3都
+    <div class="alert alert-primary" role="alert">
+      A simple primary alert—check it out!
+    </div>
+    <div class="alert alert-secondary" role="alert">
+      A simple secondary alert—check it out!
+    </div>
+    <div class="alert alert-success" role="alert">
+      A simple success alert—check it out!
+    </div>
+    <div class="alert alert-danger" role="alert">
+      A simple danger alert—check it out!
+    </div>
+    <div class="alert alert-warning" role="alert">
+      A simple warning alert—check it out!
+    </div>
+    <div class="alert alert-info" role="alert">
+      A simple info alert—check it out!
+    </div>
+    <div class="alert alert-light" role="alert">
+      A simple light alert—check it out!
+    </div>
+    <div class="alert alert-dark" role="alert">
+      A simple dark alert—check it out!
+    </div>
   </div>
 </template>
 
