395d7294330d40b51d25207942932259d5d90b98
zy <1824762582@qq.com>
2024-03-17 13:26:45 +0800

    关于我们页面的微调

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 //导入Vue组件
 import IndexView from "@/views/IndexVue.vue";
-import AboutView from "@/views/About.vue";
+import AboutView from "@/views/AboutVue.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
