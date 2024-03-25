2f39a576d8e9f52e78378850dad96742d435c9df
Fine974 <1370376731@qq.com>
2024-03-25 11:16:46 +0800

    页面切换测试

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,35 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 //导入Vue组件
-import IndexVue from "@/views/IndexVue.vue";
+import MainVue from "@/views/MainVue.vue";
 import AboutVue from "@/views/AboutVue.vue";
 import ThreeVue from "@/views/ThreeVue.vue";
 import UploadVue from "@/views/UploadVue.vue";
+import Page from "@/views/Page.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      children: [
-        {
-          path: '',
-          component: IndexVue,
-        },
-        {
-          path: '/about',
-          component: AboutVue,
-        },
-        {
-          path: '/three',
-          component: ThreeVue,
-        },
-        {
-          path: '/upload',
-          component: UploadVue,
-        },
-      ],
+      component: MainVue,
     },
+    {
+      path: '/about',
+      component: AboutVue,
+    },
+    {
+      path: '/three',
+      component: ThreeVue,
+    },
+    {
+      path: '/upload',
+      component: UploadVue,
+    },
+    { path: '/page/:id', name: 'Page', component: Page },
   ]
 })
 
