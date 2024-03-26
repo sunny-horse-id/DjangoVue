a5aedc5f0b0fa764b1b74adf4058ce5f46d4ab18
zy <1824762582@qq.com>
2024-03-26 17:26:44 +0800

    终极版微调

--- frontend/src/views/IndexVue.vue
+++ frontend/src/views/IndexVue.vue
@@ -193,44 +193,5 @@ watch(address, (newValue) => {
 </script>
 
 <style>
-// 标签页样式
-.demo-tabs {
-  color: #6b778c;
-  font-size: 32px;
-  font-weight: 600;
-}
 
-// 单模型下载按钮样式
-.button-download {
-  position: fixed;
-  top: 20px;
-  right: 150px;
-  z-index: 999;
-}
-
-// 播放与暂停按钮样式
-.button-wrapper {
-  position: fixed;
-  top: 20px;
-  right: 20px; /* 将右侧间距调整为适当的距离 */
-  z-index: 999;
-}
-
-// 按钮基础样式
-button {
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  outline: none;
-  margin-right: 10px; /* 添加右侧间距，使其与播放按钮有一定的距离 */
-}
-
-// 鼠标悬停时的样式
-button:hover {
-  background-color: #0056b3;
-}
 </style>
