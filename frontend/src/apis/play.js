ff9372acd5e6672bc3c19dc745f4e5da9b227191
zy <1824762582@qq.com>
2024-03-26 17:21:13 +0800

    终极版微调

--- frontend/src/apis/play.js
+++ frontend/src/apis/play.js
@@ -1,9 +1,5 @@
 import request from "@/utils/request.js"
 
-export const PlayAddress = () => {
-    return request.get('/address')
-}
-
 export const PlayMulAddress = () => {
     return request.get('/mulAddress')
 }
